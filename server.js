1328a21eb288193db47de07265ee127762297d37
Mikesh Patel <mikeshpatel39@gmail.com>
2022-08-12 12:27:00 +1200

    add random fish generator and prototype counter not correctly routed

--- server.js
+++ server.js
@@ -16,6 +16,36 @@ server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
 
-module.exports = server
+const dataPath = path.join(__dirname, 'data.json')
 
-server.use('/route-name-here', routes)
+let clickCounter = 0
+const randomNo = Math.floor(Math.random() * 3)
+
+server.get('/game', (req, res) => {
+  const contents = fs.readFile(dataPath, 'utf-8')
+  const data = JSON.parse(contents)
+  const fishObject = data.theFish
+  const fish = fishObject[randomNo]
+  const fishImage = fish.image
+  fishImage.onclick = clickCounter++
+  const viewData = {
+    image: fishImage,
+  }
+  res.render('game', viewData)
+})
+
+server.get('/game/details', (req, res) => {
+  const contents = fs.readFile(dataPath, 'utf-8')
+  const data = JSON.parse(contents)
+  const fishObject = data.theFish
+  const fish = fishObject[randomNo]
+  const viewData = {
+    image: fish.image,
+    name: fish.name,
+    home: fish.home,
+  }
+  if (clickCounter === 10) {
+    res.render('details', viewData)
+  }
+})
+module.exports = server
